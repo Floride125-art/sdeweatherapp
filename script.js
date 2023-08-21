3a162e18a940f069cebc542806bafe25d53e121a
Floride125-art <flo.tuyisenge@gmail.com>
2023-08-21 12:32:01 +0530

    create apikey and declared variabled to us

---
+++ script.js
@@ -0,0 +1,6 @@
+const apiKey = "54204f2e00c4d3d6a86ef1348e3aba97";
+
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+  
